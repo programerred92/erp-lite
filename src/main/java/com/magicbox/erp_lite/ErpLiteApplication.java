c87f9aa8e0c05cd239c0c55ad24d0c5e665c389f
programerred92 <haprorold@gmail.com>
2026-05-08 05:17:20 -0500

    Initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/com/magicbox/erp_lite/ErpLiteApplication.java
@@ -0,0 +1,13 @@
+package com.magicbox.erp_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLiteApplication.class, args);
+	}
+
+}
